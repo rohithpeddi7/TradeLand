6b6fdb6a971cc8f30869874734b320083592eed7
rohithpeddi7 <peddi.rohith.cse21@itbhu.ac.in>
2022-11-06 00:05:48 +0530

    Users app

---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class users(models.Model):
+    login_id = models.ForeignKey(User,on_delete=models.CASCADE,null=False)
+    gender = models.CharField(max_length=1,default='M')
